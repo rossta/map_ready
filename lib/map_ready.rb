5b04ba2ecc7dafc62f5363bdfda433438f4083f9
Ross Kaffenberger <rosskaff@gmail.com>
2011-01-04 15:01:30 -0500

    require geokit-rails

--- lib/map_ready.rb
+++ lib/map_ready.rb
@@ -3,8 +3,7 @@
 $:.unshift(File.dirname(__FILE__ + '.rb') + '/../lib') unless $:.include?(File.dirname(__FILE__ + '.rb') + '/../lib')
 require 'activerecord'
 require 'geokit'
-require File.dirname(__FILE__) + "/../../geokit-rails/lib/geokit-rails/defaults.rb"
-require File.dirname(__FILE__) + "/../../geokit-rails/lib/geokit-rails/acts_as_mappable.rb"
+require 'geokit-rails'
 ActiveRecord::Base.send :include, GeoKit::ActsAsMappable
 
 require "marker_builder"
